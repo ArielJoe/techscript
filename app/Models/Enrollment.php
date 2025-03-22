ebfdd7c23dfffd7c0b0def950aab78e182d5e244
Eben <2372023@maranatha.ac.id>
2025-03-22 08:20:35 +0700

    Check model

--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Enrollment extends Model
 {
-    //
+    protected $table = 'enrollment';
+
+    protected $primaryKey = 'id';
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id',
+        'Course_id',
+        'Student_id'
+    ];
 }
