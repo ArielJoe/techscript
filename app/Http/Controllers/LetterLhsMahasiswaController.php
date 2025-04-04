e8489abcd84da377b1d0da4713bff0d153315699
Eben <2372023@maranatha.ac.id>
2025-04-04 10:52:42 +0700

    completing SPTMK and LHS (index, create, store, show, if null data, etc) section

--- app/Http/Controllers/LetterLhsMahasiswaController.php
+++ app/Http/Controllers/LetterLhsMahasiswaController.php
@@ -3,16 +3,47 @@
 namespace App\Http\Controllers;
 
 use App\Models\Letter;
+use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use DateTime;
+use Illuminate\Http\RedirectResponse;
+use Exception;
+use Carbon\Carbon;
 
 class LetterLhsMahasiswaController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('/mahasiswa/lhs/index');
+        $search = $request->search;
+
+        $letters = Letter::where('Student_id', 'STU' . Auth::id())
+            ->where('category', 'LHS')
+            ->when($search, function ($query, $search) {
+                return $query->where('id', 'like', "%{$search}%");
+            })
+            ->latest()->paginate(10);
+        $student = Student::where('User_id', Auth::id())->first();
+
+        foreach ($letters as $letter) {
+            $letter->date_indo = Carbon::parse($letter->created_at)->locale('id')->translatedFormat('d F Y');
+            $letter->status_text = match ($letter->status) {
+                1 => 'Ditolak',
+                2 => 'Diproses',
+                3 => 'Diterima'
+            };
+            $letter->file_path = $letter->file_path;
+            // Get student data
+            $letter->full_name = $student->full_name;
+            $letter->nrp = $student->id;
+            $letter->address = $student->address;
+        }
+
+        return view('/mahasiswa/lhs/index')->with('letters', $letters);
     }
 
     /**
@@ -20,7 +51,26 @@ class LetterLhsMahasiswaController extends Controller
      */
     public function create()
     {
-        //
+        $userId = Auth::id();
+        $students = DB::select("
+            SELECT 
+                Student.id AS student_id,
+                Student.full_name,
+                Course.period AS period,
+                Major.name AS program_studi
+            FROM 
+                Student
+            LEFT JOIN 
+                Enrollment ON Student.id = Enrollment.Student_id
+            LEFT JOIN 
+                Course ON Enrollment.Course_id = Course.id
+            LEFT JOIN 
+                Major ON Course.Major_id = Major.id
+            WHERE 
+                Student.user_id = ?
+        ", [$userId]);
+        
+        return view('/mahasiswa/lhs/create')->with('students', $students);
     }
 
     /**
@@ -28,7 +78,60 @@ class LetterLhsMahasiswaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // validate form
+        $request->validate([
+            'purpose'   => 'required|string|max:100'
+        ]);
+
+        function generateLetterNumber()
+        {
+            $letterCategory = 'LHS';
+            $userId = Auth::id();
+
+            $currentYear = Date('Y');
+            $user = DB::table('User')->where('id', $userId)->first();
+            if (!$user) {
+                throw new Exception('User not found');
+            }
+            $majorId = $user->Major_id;
+            $major = DB::table('Major')->where('id', $majorId)->first();
+            if (!$major) {
+                throw new Exception('Major not found');
+            }
+            $majorName = $major->name;
+            $words = explode(' ', $majorName);
+            $abbreviation = '';
+            foreach ($words as $word) {
+                $abbreviation .= strtoupper(substr($word, 0, 1));
+            }
+            $count = DB::table('Letter')
+                ->where('category', $letterCategory)
+                ->where('Major_id', $majorId)
+                ->whereRaw('YEAR(created_at) = ?', [$currentYear])
+                ->count();
+            $letterNumber = str_pad($count + 1, 3, '0', STR_PAD_LEFT);
+            $fullLetterNumber = "$letterNumber/$letterCategory/$abbreviation/$currentYear";
+
+            return $fullLetterNumber;
+        }
+
+        // create request letter
+        $userId = Auth::id();
+        $user = DB::table('User')->where('id', $userId)->first();
+        if (!$user) {
+            throw new Exception('User not found');
+        }
+
+        $majorId = $user->Major_id;
+        Letter::create([
+            'id'       => generateLetterNumber(),
+            'category' => 'LHS',
+            'purpose' => $request->purpose,
+            'status'   => 2,
+            'Student_id' => 'STU' . Auth::id(),
+            'Major_id' => $majorId,
+        ]);
+        return redirect()->route('mahasiswa.lhs.index')->with(['success' => 'LHS telah diajukan!']);
     }
 
     /**
@@ -36,7 +139,18 @@ class LetterLhsMahasiswaController extends Controller
      */
     public function show(Letter $letter)
     {
-        //
+        $letters = Letter::where('Student_id', 'STU' . Auth::id())
+                        ->where('category', 'LHS')->latest()->get();
+        foreach ($letters as $letter) {
+            $letter->date_indo = Carbon::parse($letter->created_at)->locale('id')->translatedFormat('d F Y');
+            $letter->status_text = match ($letter->status) {
+                1 => 'Ditolak',
+                2 => 'Diproses',
+                3 => 'Diterima'
+            };
+        }
+
+        return view('/mahasiswa/lhs/show')->with('letters', $letters);
     }
 
     /**
--- app/Http/Controllers/LetterSptmkMahasiswaController.php
+++ app/Http/Controllers/LetterSptmkMahasiswaController.php
@@ -3,16 +3,48 @@
 namespace App\Http\Controllers;
 
 use App\Models\Letter;
+use App\Models\Student;
+use App\Models\Course;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use DateTime;
+use Illuminate\Http\RedirectResponse;
+use Exception;
+use Carbon\Carbon;
 
 class LetterSptmkMahasiswaController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('/mahasiswa/sptmk/index');
+        $search = $request->search;
+
+        $letters = Letter::where('Student_id', 'STU' . Auth::id())
+            ->where('category', 'SPTMK')
+            ->when($search, function ($query, $search) {
+                return $query->where('id', 'like', "%{$search}%");
+            })
+            ->latest()->paginate(10);
+        $student = Student::where('User_id', Auth::id())->first();
+
+        foreach ($letters as $letter) {
+            $letter->date_indo = Carbon::parse($letter->created_at)->locale('id')->translatedFormat('d F Y');
+            $letter->status_text = match ($letter->status) {
+                1 => 'Ditolak',
+                2 => 'Diproses',
+                3 => 'Diterima'
+            };
+            $letter->file_path = $letter->file_path;
+            // Get student data
+            $letter->full_name = $student->full_name;
+            $letter->nrp = $student->id;
+            $letter->address = $student->address;
+        }
+
+        return view('/mahasiswa/sptmk/index')->with('letters', $letters);
     }
 
     /**
@@ -20,7 +52,22 @@ class LetterSptmkMahasiswaController extends Controller
      */
     public function create()
     {
-        //
+        $userId = Auth::id();
+
+        $major = DB::table('User')
+        ->join('Major', 'User.Major_id', '=', 'Major.id')
+        ->where('User.id', $userId)
+        ->select('Major.id as major_id', 'Major.name as major_name')
+        ->first();
+
+        $courses = DB::table('Enrollment')
+            ->join('Course', 'Enrollment.Course_id', '=', 'Course.id')
+            ->join('Student', 'Enrollment.Student_id', '=', 'Student.id')
+            ->where('Student.User_id', $userId) // Menyesuaikan dengan user yang sedang login
+            ->select('Course.id as course_id', 'Course.name as course_name', 'Course.period')
+            ->get();
+
+        return view('/mahasiswa/sptmk/create')->with('courses', $courses)->with('major', $major);
     }
 
     /**
@@ -28,7 +75,68 @@ class LetterSptmkMahasiswaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // validate form
+        $request->validate([
+            'selected_course'   => 'required|string|max:50',
+            'purpose'           => 'required|string|max:100',
+            'topic'             => 'required|string|max:100',
+            'addressed_to'      => 'required|string|max:200',
+            'list_description'  => 'required|string|max:200'
+        ]);
+
+        function generateLetterNumber()
+        {
+            $letterCategory = 'SPTMK';
+            $userId = Auth::id();
+
+            $currentYear = Date('Y');
+            $user = DB::table('User')->where('id', $userId)->first();
+            if (!$user) {
+                throw new Exception('User not found');
+            }
+            $majorId = $user->Major_id;
+            $major = DB::table('Major')->where('id', $majorId)->first();
+            if (!$major) {
+                throw new Exception('Major not found');
+            }
+            $majorName = $major->name;
+            $words = explode(' ', $majorName);
+            $abbreviation = '';
+            foreach ($words as $word) {
+                $abbreviation .= strtoupper(substr($word, 0, 1));
+            }
+            $count = DB::table('Letter')
+                ->where('category', $letterCategory)
+                ->where('Major_id', $majorId)
+                ->whereRaw('YEAR(created_at) = ?', [$currentYear])
+                ->count();
+            $letterNumber = str_pad($count + 1, 3, '0', STR_PAD_LEFT);
+            $fullLetterNumber = "$letterNumber/$letterCategory/$abbreviation/$currentYear";
+
+            return $fullLetterNumber;
+        }
+
+        // create request letter
+        $userId = Auth::id();
+        $user = DB::table('User')->where('id', $userId)->first();
+        if (!$user) {
+            throw new Exception('User not found');
+        }
+
+        $majorId = $user->Major_id;
+        Letter::create([
+            'id'       => generateLetterNumber(),
+            'category' => 'SPTMK',
+            'selected_course' => $request->selected_course,
+            'purpose' => $request->purpose,
+            'topic' => $request->topic,
+            'addressed_to' => $request->addressed_to,
+            'list_description' => $request->list_description,
+            'status'   => 2,
+            'Student_id' => 'STU' . Auth::id(),
+            'Major_id' => $majorId,
+        ]);
+        return redirect()->route('mahasiswa.sptmk.index')->with(['success' => 'SPTMK telah diajukan!']);
     }
 
     /**
